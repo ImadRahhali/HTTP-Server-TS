fe5a2979c21cd9d734589bda95d3cc42bb96a563
Imad Rahhali <imad.rahhali@oracle.com>
2025-10-02 21:29:59 +0100

    updating the server to make use of the new router

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,21 @@ import {
   buildHttpResponse,
   serializeHttpResponse,
 } from "./httpResponseBuilder.ts";
+import { Router } from "./router.ts";
+import type { HttpRequest } from "./types.ts";
+
+const router = new Router();
+
+// Register routes
+router.register("GET", "/", (req: HttpRequest) =>
+  buildHttpResponse(200, "<h1>Home Page</h1>", { "Content-Type": "text/html" })
+);
+
+router.register("GET", "/about", (req: HttpRequest) =>
+  buildHttpResponse(200, "<h1>About Page</h1>", { "Content-Type": "text/html" })
+);
+
+// Server
 export const server = net.createServer((socket: net.Socket) => {
   console.log(
     `[SERVER] Client connected: ${socket.remoteAddress}:${socket.remotePort}`
@@ -14,27 +29,20 @@ export const server = net.createServer((socket: net.Socket) => {
     console.log("[SERVER] Received data from Client:", request);
 
     if (!request) {
-      const response = buildHttpResponse(400, "Bad Request");
-      socket.end(serializeHttpResponse(response));
+      socket.write(
+        serializeHttpResponse(buildHttpResponse(400, "<h1>Bad Request</h1>"))
+      );
       return;
     }
 
-    if (request.path === "/index.html") {
-      const response = buildHttpResponse(200, "<h1>Hello</h1>", {
-        "Content-Type": "text/html",
-      });
-      socket.write(serializeHttpResponse(response));
-    } else {
-      const response = buildHttpResponse(404, "Not Found");
-
-      socket.write(serializeHttpResponse(response));
-    }
+    const response = router.handle(request);
+    socket.write(serializeHttpResponse(response));
   });
 
   socket.on("end", () => {
     console.log("[SERVER] Client disconnected");
   });
   socket.on("error", (err) => {
-    console.error("[SERVER] Server Socket error:", err);
+    console.error("[SERVER] Server Socket error:", err.message);
   });
 });
