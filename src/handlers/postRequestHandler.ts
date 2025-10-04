4124f995d81bbf724fe5493965295ec4848a551f
Imad Rahhali <imad.rahhali@oracle.com>
2025-10-04 14:14:22 +0100

    modify post request handler to handle Keep-alive connections

--- src/handlers/postRequestHandler.ts
+++ src/handlers/postRequestHandler.ts
@@ -9,8 +9,11 @@ export async function servePostRequest(req: HttpRequest, socket: net.Socket) {
   const body = req.body || "<empty>";
   const response = buildHttpResponse(200, `You sent: ${body}`, {
     "Content-Type": "text/plain",
-    "Content-Length": Buffer.byteLength(body).toString(),
+    Connection: req.headers["connection"] || "close",
   });
   socket.write(serializeHttpResponse(response));
-  socket.end();
+
+  if ((req.headers["connection"]?.toLowerCase() ?? "") !== "keep-alive") {
+    socket.end();
+  }
 }
